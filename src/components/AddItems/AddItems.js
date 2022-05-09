b5165f1d26fe3e55f7bcaa4a91a5b80fc492cd78
ParvesPR <parves2585@gmail.com>
2022-05-09 12:46:34 +0600

    my item delete function added

--- src/components/AddItems/AddItems.js
+++ src/components/AddItems/AddItems.js
@@ -1,8 +1,11 @@
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
+import auth from '../../firebase.init';
 
 const AddItems = () => {
     const { register, handleSubmit } = useForm();
+    const [user] = useAuthState(auth)
 
     const handleAddItem = data => {
         console.log(data)
@@ -28,13 +31,13 @@ const AddItems = () => {
             <div className='mt-5 py-5 login-container col-sm-8 col-lg-4 mx-auto'>
                 <h2 className='text-center text-white'>Please add a Item</h2>
                 <form onSubmit={handleSubmit(handleAddItem)} className='d-flex flex-column input-container'>
-                    <input className='form-control mb-3' placeholder='Name' {...register("name", { required: true, maxLength: 20 })} />
-                    <input className='form-control mb-3' placeholder='Email' {...register("email", { required: true})} required/>
+                    <input className='form-control mb-3' placeholder='Product Name' {...register("name", { required: true, maxLength: 20 })} />
+                    <input className='form-control mb-3' value={user.email} placeholder='Email' {...register("email", { required: true })} required />
                     <textarea className='form-control mb-3 bg-dark text-white' placeholder='Description' {...register("description")} />
                     <input className='form-control mb-2' placeholder='Price' type="number" {...register("price")} />
-                    <input className='form-control mb-3' placeholder='Supplier Name' {...register("supplierName", { required: true})} />
-                    <input className='form-control mb-3' placeholder='Stock' {...register("quantity", { required: true})} />
-                    <input className=' form-control mb-2' placeholder='Photo URL' type="text" {...register("img",)} />
+                    <input className='form-control mb-3' placeholder='Supplier Name' {...register("supplierName", { required: true })} />
+                    <input className='form-control mb-3' placeholder='Stock' {...register("quantity", { required: true })} />
+                    <input className=' form-control mb-2' placeholder='Photo URL' type="text" {...register("img", { required: true })} />
                     <input className='form-control btn btn-info' type="submit" />
                 </form>
             </div>
--- src/components/MyItems/MyItems.js
+++ src/components/MyItems/MyItems.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Table } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import './MyItems.css';
 
 const MyItems = () => {
     const [items, setItems] = useState([]);
+    const [load, setLoad] = useState(false)
     const [user] = useAuthState(auth);
     useEffect(() => {
 
@@ -18,32 +19,42 @@ const MyItems = () => {
         getItems();
 
 
-    }, [user])
+    }, [user]);
+
+    const handleDelete = product => {
+        const proceed = window.confirm('Are you sure you want to delete?');
+        if (proceed) {
+            const url = `http://localhost:5000/myitems/${product}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    setLoad(!load)
+                })
+        }
+    }
 
     return (
         <div className='mt-5 py-5 container-fluid'>
             <h1>My Items: {items.length}</h1>
             {
                 items.map(item =>
-                    <Table striped bordered hover variant="dark">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>User Name</th>
-                                <th>Item Price</th>
-                                <th>Quantity</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>{item.name}</td>
-                                <td>{item.price}</td>
-                                <td>@{item.quantity}</td>
-                            </tr>
-
-                        </tbody>
-                    </Table>
+                    <div key={item._id} className='container d-flex mx-auto bg-secondary text-white p-3'>
+                        <div className="col-lg-8 d-flex  mb-3 border-2">
+                            <img src={item.img} className='w-25' alt="" />
+                            <div className='ms-3'>
+                                <p>{item.name}</p>
+                                <p>Price: {item.price}</p>
+                                <p>Quantity: {item.quantity}</p>
+                                <p>Description: {item.description}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <button onClick={() => handleDelete(item._id)} className='btn btn-danger text-white '>X
+                            </button>
+                        </div>
+                    </div>
                 )
             }
         </div>
